7c320ad876bf352cbc823045831f654964164b6b
HRangelov <58166944+HRangelov@users.noreply.github.com>
2020-12-03 15:02:36 +0200

    common app created

--- gallery/urls.py
+++ gallery/urls.py
@@ -18,7 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from paintings.views import list_painting
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('common.urls')),
     path('paintings/', include('paintings.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- paintings/admin.py
+++ paintings/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from paintings.models import Painting, Like, Comment
+
+
+class LikeInline(admin.TabularInline):
+    model = Like
+
+
+class PaintingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type', 'name', 'year')
+    list_filter = ('type', 'year')
+    inlines = (
+        LikeInline,
+    )
+
+
+admin.site.register(Painting, PaintingAdmin)
+admin.site.register(Comment)
+admin.site.register(Like)
